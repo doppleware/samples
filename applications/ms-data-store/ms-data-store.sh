a43e3f5686cb1627d3eb2d1d7194f2d90dca2301
dosdosanjhquali <41974689+dosdosanjhquali@users.noreply.github.com>
2018-08-03 10:52:15 -0700

    add start

--- applications/ms-data-store/ms-data-store.sh
+++ applications/ms-data-store/ms-data-store.sh
@@ -31,3 +31,11 @@ echo "****************************************************************"
 cd sample_micro_services_nodejs_source/db || exit
 npm install;
 
+#!/usr/bin/env bash
+echo "****************************************************************"
+echo "Starting the node application"
+echo "****************************************************************"
+cd /home/artifacts/sample_micro_services_nodejs_source/db || exit
+
+node app.js;
+
